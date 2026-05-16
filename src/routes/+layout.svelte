127cbbf02ca193dbd985a2f4118e2d332b6b74c0
InvalidJoker <0limitdev@gmail.com>
2026-05-16 23:08:49 +0200

    add tw

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
+	import "./layout.css";
+
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<title>Linky</title>
-</svelte:head>
-
+<svelte:head><title>Linky</title></svelte:head>
 {@render children()}
